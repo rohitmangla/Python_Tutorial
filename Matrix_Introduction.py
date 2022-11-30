826d70b37b2f5b54fedeba2d440c0aaf29b21443
rohitmangla <42999815+rohitmangla@users.noreply.github.com>
2022-11-30 22:01:12 +0100

    updated

---
+++ Matrix_Introduction.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# Create a 2x2 matrix A
+A = np.array ([[1, 2], [ 3 , 4]])
+print (A)
+
+# Create a 3x3 matrix B
+B = np.array ([[2, 5, 7], [9, 6 , 2], [1, 4, 9]])
+print (B)
+
+# Create a 3x3 matrix C
+C = np.array ([[1, 2, 3], [8, 2, 5], [4, 9, 7 ]])
+print (C)
+# Addition of two matrices [D= B+C]
+D= B+C
+print ('D =', D)
